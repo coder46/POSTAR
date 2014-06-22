0381e0873d394c465cbd9275176c349ff2a71b6e
coder46 <faisal.iiit@gmail.com>
2014-06-22 22:39:23 +0400

    Built Camera part

--- app/src/main/java/info/androidhive/postar/app/MainActivity.java
+++ app/src/main/java/info/androidhive/postar/app/MainActivity.java
@@ -1,18 +1,119 @@
 package info.androidhive.postar.app;
 
+import android.app.Activity;
+import android.hardware.Camera;
+import android.os.Build;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
+    SurfaceView cameraPreview;
+    SurfaceHolder previewHolder;
+    Camera camera;
+    boolean inPreview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inPreview = false;
+
+        cameraPreview = (SurfaceView)findViewById(R.id.cameraPreview);
+        previewHolder = cameraPreview.getHolder();
+        previewHolder.addCallback(surfaceCallback);
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
+            previewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        }
+
+
+
     }
+
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        camera = Camera.open();
+    }
+
+    @Override
+    public void onPause(){
+
+        if(inPreview) {
+            camera.stopPreview();
+        }
+
+        camera.release();
+        camera=null;
+        inPreview=false;
+
+        super.onPause();
+
+
+    }
+
+    SurfaceHolder.Callback surfaceCallback=new SurfaceHolder.Callback() {
+        public void surfaceCreated(SurfaceHolder holder) {
+            try {
+                camera.setPreviewDisplay(previewHolder);
+            }
+            catch (Throwable t) {
+                Log.e("ProAndroidAR2Activity", "Exception in setPreviewDisplay()", t);
+            }
+        }
+
+        @Override
+        public void surfaceChanged(SurfaceHolder holder, int format, int width, int
+                height) {
+
+            Camera.Parameters parameters=camera.getParameters();
+            Camera.Size size=getBestPreviewSize(width, height, parameters);
+            if (size!=null) {
+                parameters.setPreviewSize(size.width, size.height);
+                camera.setParameters(parameters);
+                camera.startPreview();
+                inPreview=true;
+            }
+
+        }
+
+        @Override
+        public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+
+        }
+
+
+    };
+
+    private Camera.Size getBestPreviewSize(int width, int height,
+                                           Camera.Parameters parameters) {
+        Camera.Size result = null;
+        for (Camera.Size size : parameters.getSupportedPreviewSizes()) {
+            if (size.width <= width && size.height <= height) {
+                if (result == null) {
+                    result = size;
+                } else {
+                    int resultArea = result.width * result.height;
+                    int newArea = size.width * size.height;
+                    if (newArea > resultArea) {
+                        result = size;
+                    }
+                }
+            }
+        }
+
+        return (result);
+    }
+
+
+
 
 
     @Override
